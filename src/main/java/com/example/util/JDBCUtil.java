e798572e676ba1297e07f28e928df88cec53d2cd
22100451Yejin <22100451@handong.ac.kr>
2022-11-19 18:56:36 +0900

    project4

---
+++ src/main/java/com/example/util/JDBCUtil.java
@@ -0,0 +1,33 @@
+package com.example.util;
+
+import com.example.bean.BoardVO;
+import com.example.dao.BoardDAO;
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class JDBCUtil {
+	public static Connection getConnection(){  
+	    Connection con=null;  
+	    try{  
+	        Class.forName("com.mysql.cj.jdbc.Driver");
+	        con= DriverManager.getConnection("jdbc:mysql://walab.handong.edu:3306/p3_22100451","p3_22100451","Thooti0och");
+	    }catch(Exception e){
+	    	System.out.println(e);
+	    }  
+	    return con;  
+	}
+
+//	public static void main(String ars[]) {
+//		Connection conn = getConnection();
+//		if(conn != null)
+//			System.out.println("DB 연결됨!");
+//		else
+//			System.out.println("DB 연결중 오류 !");
+//	}
+}
